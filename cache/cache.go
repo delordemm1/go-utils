792f33af6ded7596632656787117ad1342487d6b
Emmanuel Ogbonna <46292455+delordemm1@users.noreply.github.com>
2024-02-12 19:11:53 +0100

    fix set bug

--- cache/cache.go
+++ cache/cache.go
@@ -33,7 +33,7 @@ func (s *CacheService) Get(ctx context.Context, key string, value interface{}) e
 	return s.rCache.Get(ctx, key, value)
 }
 
-func (s *CacheService) Set(ctx context.Context, key string, ttl time.Duration, value interface{}) error {
+func (s *CacheService) Set(ctx context.Context, key string, value interface{}, ttl time.Duration) error {
 	return s.rCache.Set(&cache.Item{
 		Ctx:   ctx,
 		Key:   key,
@@ -50,7 +50,7 @@ func (s *CacheService) Exists(ctx context.Context, key string) bool {
 	return s.rCache.Exists(ctx, key)
 }
 
-func (s *CacheService) Remember(ctx context.Context, key string, ttl time.Duration, val interface{}, do func(*cache.Item) (interface{}, error)) error {
+func (s *CacheService) Remember(ctx context.Context, key string, val interface{}, ttl time.Duration, do func(*cache.Item) (interface{}, error)) error {
 	if !s.Exists(ctx, key) {
 		err := s.rCache.Set(&cache.Item{
 			Ctx:   ctx,
